99dffd9ab3524274d937e4490c3726cc2d6a5de2
tarannumparwin <tarannumparwin1995@gmail.com>
2023-04-10 11:03:34 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "<secret>",
+    EMAIL: "steve.franecki@ethereal.email", // testing email & password
+    PASSWORD : "sMf46xCzrvdrxvuagc",
+    ATLAS_URI: "mongodb+srv://tarannumparwin1111:7ubtoEnbJa9PteOL@cluster0.p17hrbz.mongodb.net/test"
+}
